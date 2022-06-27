ef4edebf4eb14be0fe563292ef3a718982795e68
theamarin <theamarin@thecreation.de>
2022-06-27 21:40:11 +0200

    Paranthesis

--- src/parser.nim
+++ src/parser.nim
@@ -5,12 +5,16 @@ type
    NodeKind = enum
       node_number = "number"
       node_binary_expression = "binary expression"
+      node_paranthesis_expression = "paranthesis expression"
    Node* = ref object
       case kind: NodeKind
       of node_number: numberToken: Token
       of node_binary_expression:
          left, right: Node
          operatorToken: Token
+      of node_paranthesis_expression:
+         open, close: Token
+         expression: Node
 
    Parser = ref object
       lexer: Lexer
@@ -28,6 +32,11 @@ func `$`(node: Node): string =
       result &= indent($node.left, 3) & "\p"
       result &= indent($node.operatorToken, 3) & "\p"
       result &= indent($node.right, 3)
+   of node_paranthesis_expression:
+      result &= "\p"
+      result &= indent($node.open, 3) & "\p"
+      result &= indent($node.expression, 3) & "\p"
+      result &= indent($node.close, 3)
 
 func peek(parser: Parser, offset: int = 0): Token =
    return parser.lexer.get(parser.position + offset)
@@ -45,9 +54,18 @@ func match(parser: var Parser, kind: TokenKind): Token {.discardable.} =
          ", expected " & escape($kind))
    return Token()
 
+func parseExpression(parser: var Parser): Node
+
+
 func parsePrimaryExpression(parser: var Parser): Node =
-   let token = parser.match(token_number)
-   return Node(kind: node_number, numberToken: token)
+   if parser.current.kind == token_paranthesis_open:
+      let open = parser.next
+      let expression = parser.parseExpression
+      let close = parser.match(token_paranthesis_close)
+      return Node(kind: node_paranthesis_expression, open: open, expression: expression, close: close)
+   else:
+      let token = parser.match(token_number)
+      return Node(kind: node_number, numberToken: token)
 
 func parseFactor(parser: var Parser): Node =
    var left = parser.parsePrimaryExpression
@@ -69,13 +87,14 @@ func parseTerm(parser: var Parser): Node =
             operatorToken: operatorToken, right: right)
    return left
 
+func parseExpression(parser: var Parser): Node = parser.parseTerm
 
 func parse*(text: string): Parser =
    var parser = Parser()
    parser.lexer = text.lex
    parser.diagnostics = parser.lexer.getDiagnostics
 
-   let left = parser.parseTerm
+   let left = parser.parseExpression
    parser.match(token_eof)
 
    parser.root = left
@@ -96,4 +115,6 @@ func evaluate*(node: Node): int =
       of token_slash: return left div right
       else: raise newException(ValueError, "Unexpected binary operator " & escape(
             $node.operatorToken.kind))
+   of node_paranthesis_expression:
+      return node.expression.evaluate
    else: raise newException(ValueError, "Unexpected node " & escape($node.kind))
